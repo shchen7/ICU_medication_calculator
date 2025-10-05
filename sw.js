d9d07379f54317c22721e8b3b2c16fabb92846bf
shchen7 <josephine78121@gmail.com>
2025-10-05 23:41:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'icu-cache-v2'; // 每次改內容就 +1
+const CACHE = 'icu-cache-v4'; 
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html','./manifest.json'])));
@@ -10,7 +10,7 @@ self.addEventListener('activate', e => {
     await self.clients.claim();
   })());
 });
-// 導航頁面用「網路優先」，避免 index 被舊版卡住
+
 self.addEventListener('fetch', e => {
   if (e.request.mode === 'navigate') {
     e.respondWith(fetch(e.request).catch(() => caches.match('./index.html')));
